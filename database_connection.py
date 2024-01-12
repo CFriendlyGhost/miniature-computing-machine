051a8dd43d21ebda23a7f3b3ae9a42c592f8954c
Kacper <kacper.dusza@icloud.com>
2024-01-12 17:58:16 +0100

    Retrieving data only for exact user.

--- database_connection.py
+++ database_connection.py
@@ -35,29 +35,38 @@ def insert_test_records():
     cursor.execute("INSERT INTO accounts VALUES(0, 1000)")
     cursor.execute("INSERT INTO accounts VALUES (1, 1000)")
     cursor.execute("INSERT INTO transactions VALUES (0, 0, 300, 0)")
+    cursor.execute("INSERT INTO transactions VALUES (2, 0, 300, 0)")
+    cursor.execute("INSERT INTO transactions VALUES (3, 0, 300, 0)")
     cursor.execute("INSERT INTO transactions VALUES (1, 1, 350, 1)")
 
     connection.commit()
     connection.close()
 
 
-@app.route('/bank/data', methods=['GET'])
-def extract_records():
+@app.route('/bank/data/<int:accountId>', methods=['GET'])
+def extract_records(accountId):
     if request.method == 'GET':
         connection = sqlite3.connect("bank.db")
         cursor = connection.cursor()
 
-        cursor.execute("SELECT * FROM accounts")
-        accounts_records = cursor.fetchall()
+        cursor.execute("SELECT * "
+                       "FROM accounts "
+                       "WHERE account_id == ?", (accountId,))
+        account = cursor.fetchall()
 
-        cursor.execute("SELECT * FROM transactions")
+        if not account:
+            connection.close()
+            return json.dumps({"error": "Account not found"}), 404
+
+        cursor.execute("SELECT * "
+                       "FROM transactions "
+                       "WHERE account_id == ?", (accountId,))
         transactions_records = cursor.fetchall()
 
         connection.close()
 
-        accounts = [
-            {"idKonta": record[0], "bilans": record[1]} for record in accounts_records
-        ]
+        account_dict = {"idKonta": account[0][0], "bilans": account[0][1]}
+
         transactions = [
             {
                 "idTransakcji": record[0],
@@ -68,7 +77,7 @@ def extract_records():
             for record in transactions_records
         ]
 
-        return json.dumps({"accounts": accounts, "transactions": transactions})
+        return json.dumps({"account": account_dict, "transactions": transactions})
 
 
 if __name__ == "__main__":
