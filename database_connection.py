2714d25e7ddd904d0116fa47cb55fdd9cc05cc33
Kacper <kacper.dusza@icloud.com>
2024-01-13 01:07:47 +0100

    Update database method added, will be completed soon.

--- database_connection.py
+++ database_connection.py
@@ -43,24 +43,60 @@ def insert_test_records():
     connection.close()
 
 
-@app.route('/bank/data/<int:accountId>', methods=['GET'])
+def create_new_account(account_id):
+    connection = sqlite3.connect("bank.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO accounts VALUES(?, 1000)", (account_id,))
+    connection.commit()
+    connection.close()
+
+
+def update_database(account_id, isDeposit, amount):
+    connection = sqlite3.connect("bank.db")
+    cursor = connection.cursor()
+    is_transaction_successful = False
+
+    cursor.execute("SELECT * FROM accounts WHERE account_id == ?", (account_id,))
+    is_account_active = len(cursor.fetchall()) > 0
+
+    if not is_account_active:
+        create_new_account(account_id)
+
+    if isDeposit:
+        cursor.execute("UPDATE accounts SET balance = balance + ? WHERE account_id == ?", (amount, account_id))
+        #TODO transaction
+        is_transaction_successful = True
+
+    else:
+        cursor.execute("SELECT balance FROM accounts WHERE account_id == ?", (account_id,))
+        retrieved_amount = cursor.fetchall()
+        if retrieved_amount[0][0] > amount:
+            cursor.execute("UPDATE accounts SET balance = balance - ? WHERE account_id == ?", (amount, account_id))
+            # TODO transaction
+
+    connection.commit()
+    connection.close()
+    return is_transaction_successful
+
+
+@app.route("/bank/data/<int:accountId>", methods=["GET"])
 def extract_records(accountId):
-    if request.method == 'GET':
+    if request.method == "GET":
         connection = sqlite3.connect("bank.db")
         cursor = connection.cursor()
 
-        cursor.execute("SELECT * "
-                       "FROM accounts "
-                       "WHERE account_id == ?", (accountId,))
+        cursor.execute(
+            "SELECT * " "FROM accounts " "WHERE account_id == ?", (accountId,)
+        )
         account = cursor.fetchall()
 
         if not account:
             connection.close()
             return json.dumps({"error": "Account not found"}), 404
 
-        cursor.execute("SELECT * "
-                       "FROM transactions "
-                       "WHERE account_id == ?", (accountId,))
+        cursor.execute(
+            "SELECT * " "FROM transactions " "WHERE account_id == ?", (accountId,)
+        )
         transactions_records = cursor.fetchall()
 
         connection.close()
@@ -79,12 +115,12 @@ def extract_records(accountId):
 
         response = jsonify({"account": account_dict, "transactions": transactions})
         response.headers.add('Access-Control-Allow-Origin', '*')
-        # return json.dumps({"account": account_dict, "transactions": transactions})
         return response
 
 
 if __name__ == "__main__":
     # create_database()
     # insert_test_records()
+    # print(update_database(0, False, 1000))
     app.run(debug=True)
 
